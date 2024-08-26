7a5ad101ebe9c396ab259bc3141a3a5ecddf5be0
aracelipa <137184915+aracelipa@users.noreply.github.com>
2024-08-25 20:00:12 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@ Route::get('/Crear_estudiante',[EstudianteController::class,'Crear_estudiante'])
 Route::get('/mostrar_estudiantes',[EstudianteController::class,'mostrar_estudiantes']);
 Route::get('/buscar_estudiante/{id}',[EstudianteController::class,'buscar_estudiante']);
 Route::get('/borrar_estudiante/{id}',[EstudianteController::class,'borrar_estudiante']);
-Route::get('/modificar_estudiante/{id}',[EstudianteController::class,'modificar_estudiante']);//manuelmente por codigo
+Route::get('/modificar_estudiante/{id}',[EstudianteController::class,'modificar_estudiante']);//manualmente por codigo
 
 
